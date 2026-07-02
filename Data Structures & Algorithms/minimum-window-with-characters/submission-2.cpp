c39b50a4c4aea9650bf737606dc5b0033926bcad
Utkarsh Mandloi <124374672+utkarshm12@users.noreply.github.com>
2026-07-03 05:27:39 +0530

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.cpp
@@ -0,0 +1,35 @@
+class Solution {
+
+    bool isSubset(unordered_map<char, int> super, unordered_map<char, int> sub){
+        for(auto i : sub) if(super[i.first] < i.second) return false;
+        return true;
+    }
+
+public:
+    string minWindow(string s, string t) {
+        if(s.length() < t.length()) return "";
+        
+        int n = s.size(), l = 0;
+        int mn = INT_MAX;
+        unordered_map<char, int> ft, fs;
+        string temp = "", ans = "";
+
+        for(auto c : t) ft[c]++;
+
+        for(int i = 0; i < n; i++){
+            fs[s[i]]++;
+            temp += s[i];
+            while(isSubset(fs, ft)){
+                if(i - l + 1 < mn){
+                    mn = i-l+1;
+                    ans = temp;
+                }
+                fs[s[l++]]--;
+                temp.erase(0, 1);
+            }
+        }
+
+        
+        return ans;
+    }
+};
