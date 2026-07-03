d4caef608501121118146342a59beb491a7cf694
Utkarsh Mandloi <124374672+utkarshm12@users.noreply.github.com>
2026-07-04 01:15:11 +0530

    Add: car-fleet - submission-7

---
+++ Data Structures & Algorithms/car-fleet/submission-7.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        vector<pair<int, int>> mp;
+        vector<double> fleets;
+
+        for(int i = 0; i < speed.size(); i++) mp.push_back({target - position[i], speed[i]});
+        sort(mp.begin(), mp.end(), [] (auto a, auto b){
+            return a.first < b.first;
+        });
+
+        for(auto i : mp){
+            fleets.push_back((double)i.first/i.second);
+        }
+
+        int c = 1;
+        double mx = fleets[0];
+
+        cout << fleets[0] << " ";
+        for(int i = 1; i < fleets.size(); i++){
+            cout << fleets[i] << " ";
+            if(fleets[i] > mx){
+                mx = fleets[i];
+                c++;
+            }
+        }
+
+        return c;
+
+    }
+};
