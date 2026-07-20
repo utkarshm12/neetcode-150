facdfae2fc57ecb95d1d36d8f160adce5f6d01ca
Utkarsh Mandloi <124374672+utkarshm12@users.noreply.github.com>
2026-07-21 03:38:01 +0530

    Add: combinations-of-a-phone-number - submission-2

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-2.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    unordered_map<char, vector<char>> mp = {
+        {'2', {'a', 'b', 'c'}},
+        {'3', {'d', 'e', 'f'}}, 
+        {'4', {'g', 'h', 'i'}},
+        {'5', {'j', 'k', 'l'}},
+        {'6', {'m', 'n', 'o'}},
+        {'7', {'p', 'q', 'r', 's'}},
+        {'8', {'t', 'u', 'v'}},
+        {'9', {'w', 'x', 'y', 'z'}}
+    };
+    vector<string> ans;
+public:
+    vector<string> letterCombinations(string digits) {
+        if(digits.size() == 0) return {};
+        sol(digits, 0, "");
+        return ans;    
+    }
+
+    void sol(string& digits, int id, string s){
+        if(s.size() == digits.size()){
+            ans.push_back(s);
+            return;
+        }
+
+        for(int i = 0; i < mp[digits[id]].size(); i++) sol(digits, id+1, s + mp[digits[id]][i]);
+    }
+};
