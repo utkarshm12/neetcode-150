3718b6616e983f43c917483f8ec705b844e24b4f
Utkarsh Mandloi <124374672+utkarshm12@users.noreply.github.com>
2026-07-27 03:57:39 +0530

    Add: count-connected-components - submission-0

---
+++ Data Structures & Algorithms/count-connected-components/submission-0.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    unordered_map<int, vector<int>> list;
+    vector<int> visited;
+public:
+    int countComponents(int n, vector<vector<int>>& edges) {
+        int ans = 0;
+        visited.resize(n, 0);
+
+        for(auto i : edges){
+            list[i[0]].push_back(i[1]);
+            list[i[1]].push_back(i[0]);
+        }
+
+        for(int i = 0; i < n; i++){
+            if(!visited[i]){
+                ans++;
+                visit(i);
+            }
+        }
+
+        return ans;
+    }
+
+    void visit(int i){
+        visited[i] = 1;
+
+        for(auto j : list[i]) if(!visited[j]) visit(j);
+    }
+};
